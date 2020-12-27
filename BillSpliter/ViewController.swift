ebbce966d34fab43100b0fc26ba924c803fb3352
João Victor Ipirajá <joaovictorpiraja@gmail.com>
2020-12-27 11:17:53 -0300

    Initial Commit

---
+++ BillSpliter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BillSpliter
+//
+//  Created by João Victor Ipirajá de Alencar on 27/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
